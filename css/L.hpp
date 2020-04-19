025f4458765408cf169b1f0e0651028986c2b96e
Dan-sensei <datrixz997@gmail.com>
2020-04-19 23:14:27 +0200

    Ocultasión

---
+++ css/L.hpp
@@ -0,0 +1,8 @@
+BOOTH:
+
+-Illustrious Póster     - ¥ 3300    =   30€     https://setoran.booth.pm/items/1484610
+-Sirius Póster          - ¥ 3800    =   32€     https://setoran.booth.pm/items/1508322
+
+-Touhou Pache Póster    - ¥ 3300    =   30€     https://setoran.booth.pm/items/1488803
+-Atago Towel            - ¥ 4000    =   34€     https://tengukotengu.booth.pm/items/1509207
+
